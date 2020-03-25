78aa2316a7372bd7e4600dd473114d4511fb603e
RubanenkoV <58641047+RubanenkoV@users.noreply.github.com>
2020-03-25 13:45:31 +0200

    Add files via upload

---
+++ task7.7.py
@@ -0,0 +1,22 @@
+n = int(input())
+arr = []
+for i in range(n):
+    i = int(input())
+    arr.append(i)
+
+def insertion_sort(array):
+    n = len(array)
+    for index in range(1, n):
+        CurrentValue = array[index]
+        position = index
+        while position > 0:
+            if array[position - 1] > CurrentValue:
+                array[position] = array[position - 1]
+            else:
+                break
+            position -= 1
+
+        array[position] = CurrentValue
+
+insertion_sort(arr)
+print(arr)
